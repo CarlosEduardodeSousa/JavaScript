2a3a13f8d82744311d7ed65c3be93be0d9ec428c
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2026-08-14 16:37:31 -0300

    Manipulação de dados com JS

---
+++ Aula06/script.js
@@ -0,0 +1,2 @@
+var nome = window.prompt("Qual é o seu nome?")
+alert("É um grande prazer de te conhecer, " + nome)
---
+++ Aula07/script.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt("Digite o primeiro número:"))
+var n2 = Number(window.prompt("Digite o segundo número:"))
+var resultado = n1 + n2
+alert("O resultado é: " + String(resultado))
