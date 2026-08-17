2aa54925ce2efbee4b1a074f084ecc669677e392
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2026-08-17 12:22:11 -0300

    Variáveis e Tratamento de Dados

--- Aula06/script.js
+++
@@ -1,2 +0,0 @@
-var nome = window.prompt("Qual é o seu nome?")
-alert("É um grande prazer de te conhecer, " + nome)
--- Aula07/script.js
+++
@@ -1,4 +0,0 @@
-var n1 = Number(window.prompt("Digite o primeiro número:"))
-var n2 = Number(window.prompt("Digite o segundo número:"))
-var resultado = n1 + n2
-alert("O resultado é: " + String(resultado))
