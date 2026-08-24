0ca0268aecac06ca07f619f68ddc397b014a6de4
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2026-08-24 16:06:24 -0300

    Update ex008.js

--- Aula11/ex008.js
+++ Aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 78
 console.log(`A velocidade do seu carro é ${vel} Km/h`)
-if (vel >= 60){ // Condição Simples
+if (vel > 60){ // Condição Simples
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 
