7c1550fc88c3625f89c66ed29f2d23bb8cae14a5
CarlosEduardodeSousa <carloseduardosousa378@gmail.com>
2026-08-28 12:04:31 -0300

    Estruturas de Repetição

---
+++ Aula13/ambiente.js
@@ -0,0 +1,13 @@
+var i = 1
+
+while(i <= 10){
+    console.log(`passo ${i}`)
+    i++
+}
+
+var i = 1
+
+do{
+    console.log(`${i}`)
+    i++
+}while(i <=6)
---
+++ Aula14/ambiente.js
@@ -0,0 +1,9 @@
+/*for(var i = 1; i <=10; i++){
+    console.log(`${i}`)
+}*/
+
+console.log(`Contagem...`)
+for(var i = 1; i<=10; i++){
+    console.log(i)
+}
+console.log(`Fim!`)
